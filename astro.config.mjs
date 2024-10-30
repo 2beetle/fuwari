a05af2a36c9bd538b266ab7715aa067d9c0c8e66
be <liuzhaohong@molbreeding.com>
2024-10-30 10:15:25 +0800

    depoly

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://2beetle.github.io",
-  base: "/fuwari",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
