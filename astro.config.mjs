ee0657ebd75b254051f8f75b8144e4e8664d0110
2beetle <74488739+2beetle@users.noreply.github.com>
2024-10-30 10:02:08 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://2beetle.github.io",
+  base: "/fuwari",
   trailingSlash: "always",
   integrations: [
     tailwind(),
