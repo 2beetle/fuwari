063ec301adf79c3e098008f8b2ef7b3f5904ebc2
be <liuzhaohong@molbreeding.com>
2024-10-30 12:18:13 +0800

    depoly

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://2beetle.github.io",
-  base: "/fuwari", // 修改base
+  site: "https://blog.beocean.net",
+  // base: "/fuwari", // 修改base
   trailingSlash: "always",
   integrations: [
     tailwind(),
